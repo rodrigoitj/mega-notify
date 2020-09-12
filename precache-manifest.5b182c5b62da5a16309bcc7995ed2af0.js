79c78a4f0c133ba75671b9e2739e9b8f1ebdcdd0
Rodrigo Corrêa <rodrigoitj@gmail.com>
2020-09-11 23:59:39 -0300

    Updates

---
+++ precache-manifest.5b182c5b62da5a16309bcc7995ed2af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "addbddf8a219c0e41f070da318795f10",
+    "url": "/mega-notify/index.html"
+  },
+  {
+    "revision": "24b17527c42abaeab110",
+    "url": "/mega-notify/static/css/main.153260d0.chunk.css"
+  },
+  {
+    "revision": "cec7df89ef0f437d3c0b",
+    "url": "/mega-notify/static/js/2.9062871e.chunk.js"
+  },
+  {
+    "revision": "3043e3e727d80f353557f855ab1ce92b",
+    "url": "/mega-notify/static/js/2.9062871e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b17527c42abaeab110",
+    "url": "/mega-notify/static/js/main.3104842b.chunk.js"
+  },
+  {
+    "revision": "2dd698a0ebc3a547ba99",
+    "url": "/mega-notify/static/js/runtime-main.e7ecc7e0.js"
+  }
+]);
